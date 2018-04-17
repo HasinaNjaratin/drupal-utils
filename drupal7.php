2157564925c03c7a458b5752c269b44a469ccfd7
Hasina Njaratin <hasina.mailto@gmail.com>
2018-04-17 20:21:32 +0400

    get fields value without entity_load

--- drupal7.php
+++ drupal7.php
@@ -99,4 +99,30 @@ function _drupal_get_field_collection_value($fc_id, $entity_reference_term=null,
   return $data;
 }
 
+/**
+ * Get fields value without entity load
+ * 
+ * @param $nid
+ * 
+ * @param $fields
+ * array field_name -> {value, target_id, fid}
+ *
+ * @return obj
+ */
+function _drupal_get_fields_value($nid, $fields=[]){
+  $query = db_select('node', 'n');
+  $query->addField('n', 'nid');
+  $query->addField('n', 'type');
+  $query->addField('n', 'title');
+  if(!empty($fields)){
+    foreach ($fields as $field_name => $type) {
+      $query->addField($field_name, $field_name . '_' . $type);
+      $query->addJoin('left', 'field_data_' . $field_name, $field_name, $field_name.'.entity_id = n.nid');
+    }
+  }
+  $query->condition('n.nid', $nid);
+  $results = $query->execute()->fetchAll();
+  return $results ? $results[0] : [];
+}
+
 ?>
