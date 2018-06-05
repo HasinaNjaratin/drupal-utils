d4317093e015a12600c2405a4a12131d79dedac0
Hasina Njaratin <hasina.mailto@gmail.com>
2018-06-05 21:22:06 +0400

    entity create

--- drupal7/utils.php
+++ drupal7/utils.php
@@ -39,38 +39,6 @@ function _drupal_get_data_webservice($url){
 }
 
 /**
- *  Script to create entity element
- * 
- *  @param $entity_type (node, user, taxonomy_term)
- * 
- *  @param $bundle type
- * 
- *  @param $value array [fieldname => fieldvalue]
- *  
- *  @return id
- */
-function _drupal_create_entity($entity_type, $bundle, $value = []){
-  
-}
-
-/**
- *  Script to update entity element
- * 
- *  @param $entity_type (node, user, taxonomy_term)
- * 
- *  @param $bundle type
- * 
- *  @param $id (int) entity id
- * 
- *  @param $data array [fieldname => fieldvalue]
- *  
- *  @return id
- */
-function _drupal_update_entity($entity_type, $bundle, $id, $data){
-
-}
-
-/**
  * Function to get the data_type value of the field
  *
  * @param $entity_type
@@ -101,4 +69,76 @@ function _drupal_get_field_data_value_type($field_name) {
   }
 
   return $field_type;
+}
+
+/**
+ *  Script to create entity element
+ * 
+ *  @param $entity_type (node, user, taxonomy_term)
+ * 
+ *  @param $bundle type
+ * 
+ *  @param $value array [fieldname => fieldvalue]
+ *  
+ *  @return id
+ */
+function _drupal_create_entity($entity_type, $bundle, $value = []){
+  // Create entity
+  try {
+    switch($entity_type){
+      case 'node':
+        $entity = new stdClass();
+        $entity->type = $bundle;
+        node_object_prepare($entity);
+        $entity->language = language_default('language');
+        break;
+      case 'taxonomy_term':
+        $vocabulary = taxonomy_vocabulary_machine_name_load($bundle);
+        $entity = new stdClass();
+        $entity->type = $bundle;
+        $entity->vid = $vocabulary->vid;
+        $entity->vocabulary_machine_name = $vocabulary->machine_name;
+        break;
+      case 'user':
+        $entity = new stdClass();
+        break;
+      default:
+        return false;
+        break;
+    }
+    
+    // Prepare data
+    foreach($value as $field_name => $field_value){
+      if($field_value || ($field_name == 'status')){
+        if(in_array($field_name,['name','title','mail','roles', 'status'])){
+          $entity->{$field_name} = $field_value;
+        }else{
+          $field_settings = _drupal_get_field_data_value_type($entity_type, $field_name, $bundle);
+          $field_type = $field_settings['type'];
+          $entity->{$field_name}[LANGUAGE_NONE][][$field_type] = $field_value;
+        }
+      }
+    }
+    
+    // save
+    $entity->save();
+    return $entity->id();  
+  } catch(Exception $e) {}
+}
+
+/**
+ *  Script to update entity element
+ * 
+ *  @param $entity_type (node, user, taxonomy_term)
+ * 
+ *  @param $bundle type
+ * 
+ *  @param $id (int) entity id
+ * 
+ *  @param $data array [fieldname => fieldvalue]
+ *  
+ *  @return id
+ */
+function _drupal_update_entity($entity_type, $bundle, $id, $data){
+
 }
